461d5a25f336c47ab957c2be0e5fd31884b93158
lmsnc9527 <1427700230@qq.com>
2021-12-16 14:12:33 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts.Events;
+using WpfLiveChartsTestApp.ViewModel;
+
+namespace WpfLiveChartsTestApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑 
+    /// </summary>
+    public partial class MainWindow : IDisposable
+    {
+        public MainWindow()
+
+        {
+            InitializeComponent();
+            //DataContext = new GearedViewModel();
+        }
+        //调节X轴缩放比例
+        private void Axis_OnRangeChanged(RangeChangedEventArgs eventargs)
+        {
+            var vm = (GearedViewModel)DataContext;
+
+            var currentRange = eventargs.Range;
+
+            if (currentRange < TimeSpan.TicksPerDay * 2)
+            {
+                vm.Formatter = x => new DateTime((long)x).ToString("t");    
+                return;
+            }
+
+            if (currentRange < TimeSpan.TicksPerDay * 60)
+            {
+                
+                vm.Formatter = x => new DateTime((long)x).ToString("yyyy/M/dd");
+                return;
+            }
+
+            if (currentRange < TimeSpan.TicksPerDay * 540)
+            {
+                vm.Formatter = x => new DateTime((long)x).ToString("yyyy/M");
+                return;
+            }
+            else
+            {
+                vm.Formatter = x => new DateTime((long)x).ToString("yyyy");
+                return;
+            }
+
+        }
+
+
+        public void Dispose()
+        {
+            var vm = (GearedViewModel)DataContext;
+            vm.Values.Dispose();
+        }
+    }
+}
---
+++ ViewModel/GearedViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using LiveCharts.Defaults;
+using LiveCharts.Geared;
+
+namespace WpfLiveChartsTestApp.ViewModel
+{
+    public class GearedViewModel : INotifyPropertyChanged
+    {
+        private Func<double, string> _formatter;
+        private double _from;
+        private double _to;
+
+        public GearedViewModel()
+        {
+            var now = DateTime.Now;
+            var trend = 0d;
+            var l = new List<DateTimePoint>();
+            var r = new Random();
+
+            for (var i = 0; i < 50000; i++)
+            {
+                now = now.AddHours(1);
+                l.Add(new DateTimePoint(now.AddMinutes(i), trend));
+                trend = r.NextDouble() * 10;
+                
+                //if (r.NextDouble() > 0.4)
+                //{   
+                //    trend += r.NextDouble() * 10;
+                //}
+                //else
+                //{
+                //    trend -= r.NextDouble() * 10;
+                //}
+            }
+
+            Formatter = x => new DateTime((long)x).ToString("yyyy/M/dd");
+
+            Values = l.AsGearedValues().WithQuality(Quality.High);
+
+            From = DateTime.Now.AddDays(1).Ticks;
+            To = DateTime.Now.AddDays(15).Ticks;
+        }
+
+        public object Mapper { get; set; }
+        public GearedValues<DateTimePoint> Values { get; set; }
+        public double From
+        {
+            get { return _from; }
+            set
+            {
+                _from = value;
+                OnPropertyChanged("From");
+            }
+        }
+        public double To
+        {
+            get { return _to; }
+            set
+            {
+                _to = value;
+                OnPropertyChanged("To");
+            }
+        }
+
+        public Func<double, string> Formatter
+        {
+            get { return _formatter; }
+            set
+            {
+                _formatter = value;
+                OnPropertyChanged("Formatter");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
